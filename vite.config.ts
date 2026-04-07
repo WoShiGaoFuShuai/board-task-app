aee3c988c252cf1e7b6b85d080b70aa931442f7a
Maksym Yefremov <maksym.yefremov@maklai.com.ua>
2026-04-07 18:50:31 +0530

    fix: problem with path in aliases

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(), tsconfigPaths()],
+	plugins: [vue(), tsconfigPaths({ loose: true })],
 });
