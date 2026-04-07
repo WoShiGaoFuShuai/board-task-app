19db8892af0becf057c063d20afb1da12de0e8a1
Maksym Yefremov <maksym.yefremov@maklai.com.ua>
2026-04-07 18:26:32 +0530

    feat: add aliases and fix error for corresponding type declarations.

--- src/app/router/router.ts
+++ src/app/router/router.ts
@@ -1,6 +1,5 @@
+import BoardPage from '@pages/board-page.vue';
 import { createMemoryHistory, createRouter } from 'vue-router';
-
-import BoardPage from '../../pages/board-page.vue';
 
 const routes = [{ path: '/', component: BoardPage }];
 
