1c7e4b7a0a0dd942718b06d4f30b943a09e01c59
Maksym Yefremov <maksym.yefremov@maklai.com.ua>
2026-04-08 11:42:09 +0530

    feat: add unoCss and icons

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { router } from '@app/router';
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
+import '@unocss/reset/tailwind.css';
+import 'virtual:uno.css';
 
 const pinia = createPinia();
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import vue from '@vitejs/plugin-vue';
+import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [vue(), tsconfigPaths({ loose: true })],
+	plugins: [vue(), tsconfigPaths({ loose: true }), UnoCSS()],
 });
