05837bb2bfcc379397ba613d5fe8583aa74ae138
Maksym Yefremov <maksym.yefremov@maklai.com.ua>
2026-04-10 21:06:10 +0530

    feat: add logic for getting current board

--- src/pages/board-page/ui/BoardPage.vue
+++ src/pages/board-page/ui/BoardPage.vue
@@ -1,8 +1,35 @@
 <template>
-	<div>Its board page for {{ $route.params.id }}</div>
+	<section v-if="currentBoard">
+		<div>Its board page for {{ currentBoard.title }}</div>
+	</section>
+	<!--    //TODO: зробити редірект на 404 чи fallback?  -->
+	<section v-else>Sorry, cant find the board</section>
 </template>
 
-<script setup lang="ts">
+<script
+	setup
+	lang="ts"
+>
+	import { useBoardStore } from '@entities/board';
+	import { storeToRefs } from 'pinia';
+	import { computed, onUnmounted, watchEffect } from 'vue';
+	import { useRoute } from 'vue-router';
+
+	const route = useRoute();
+	const boardStore = useBoardStore();
+	const { setCurrentBoardId, resetCurrentBoardId } = boardStore;
+	const { currentBoard } = storeToRefs(boardStore);
+
+	const paramsId = computed(() => {
+		const id = route.params.id;
+		return Array.isArray(id) ? id[0] : id;
+	});
+
+	watchEffect(() => {
+		setCurrentBoardId(paramsId.value);
+	});
+
+	onUnmounted(() => resetCurrentBoardId());
 </script>
 
 <style scoped>
